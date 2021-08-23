49816ea5bd6c04d356d79882d4e343fb33267520
Gabe <89386562+SleepyGabe@users.noreply.github.com>
2021-08-23 18:28:25 +0800

    Add files via upload

---
+++ Quadrant-Checker/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace OTFI1B_Parinas_Quadrants
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.XCoord = new System.Windows.Forms.TextBox();
+            this.YCoord = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.QuadOut = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Point:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X-Coordinate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Y-Coordinate:";
+            // 
+            // XCoord
+            // 
+            this.XCoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XCoord.Location = new System.Drawing.Point(111, 39);
+            this.XCoord.Name = "XCoord";
+            this.XCoord.Size = new System.Drawing.Size(100, 21);
+            this.XCoord.TabIndex = 3;
+            // 
+            // YCoord
+            // 
+            this.YCoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YCoord.Location = new System.Drawing.Point(111, 66);
+            this.YCoord.Name = "YCoord";
+            this.YCoord.Size = new System.Drawing.Size(100, 21);
+            this.YCoord.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Quadrant:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // QuadOut
+            // 
+            this.QuadOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuadOut.Location = new System.Drawing.Point(92, 134);
+            this.QuadOut.Name = "QuadOut";
+            this.QuadOut.ReadOnly = true;
+            this.QuadOut.Size = new System.Drawing.Size(144, 26);
+            this.QuadOut.TabIndex = 6;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.BackColor = System.Drawing.Color.LightGray;
+            this.btnProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(61, 185);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(123, 23);
+            this.btnProcess.TabIndex = 7;
+            this.btnProcess.Text = "Process Point";
+            this.btnProcess.UseVisualStyleBackColor = false;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 248);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.QuadOut);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.YCoord);
+            this.Controls.Add(this.XCoord);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox XCoord;
+        private System.Windows.Forms.TextBox YCoord;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox QuadOut;
+        private System.Windows.Forms.Button btnProcess;
+    }
+}
+
---
+++ Quadrant-Checker/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OTFI1B_Parinas_Quadrants
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            //Main Operation: Recieve inputs for X and Y coordinates and determine which quadrant they belong to.
+            //1.) Declare Variables for 1st input and 2nd input.
+            float Coord1, Coord2;
+
+            //2A.) Check text boxes for blank inputs.
+            //2B.) Declare variables for X value and Y value.
+            bool XValue, YValue;
+
+            XValue = float.TryParse(XCoord.Text, out Coord1);
+
+            if (XValue == false)
+            {
+                MessageBox.Show("Please enter a NUMBER for the X-Value.");
+                XCoord.Clear();
+                XCoord.Select();
+                return;
+            }
+
+            YValue = float.TryParse(YCoord.Text, out Coord2);
+
+            if (YValue == false)
+            {
+                MessageBox.Show("Please enter a NUMBER for the Y-Value.");
+                YCoord.Clear();
+                YCoord.Select();
+                return;
+            }
+
+            //3.) Retrieve User's inputs.
+            Coord1 = float.Parse(XCoord.Text);
+            Coord2 = float.Parse(YCoord.Text);
+
+            //4A.) Set conditions that the input must satisfy to be included in each quadrant.
+            //4B.) Check if user's input satisfies any of the conditions.
+            //4C.) Give appropriate output based on conditions satisfied.
+            if (Coord1 > 0 && Coord2 > 0)
+            {
+                QuadOut.Text = "1st Quadrant";
+            }
+            else if (Coord1 < 0 && Coord2 > 0)
+            {
+                QuadOut.Text = "2nd Quadrant";
+            }
+            else if (Coord1 < 0 && Coord2 < 0)
+            {
+                QuadOut.Text = "3rd Quadrant";
+            }
+            else if (Coord1 > 0 && Coord2 < 0)
+            {
+                QuadOut.Text = "4th Quadrant";
+            }
+            else if (Coord1 != 0 && Coord2 == 0)
+            {
+                QuadOut.Text = "On X-Axis";
+            }
+            else if (Coord1 == 0 && Coord2 != 0)
+            {
+                QuadOut.Text = "On Y-Axis";
+            }
+            else if (Coord1 == 0 && Coord2 == 0)
+            {
+                QuadOut.Text = "Point of origin";
+            }
+            else
+            {
+                return;
+            }
+
+        }
+    }
+}
